0891302e19c5940fd9b8968a755ff0960142422d
paysen <peiheng.yu@outlook.com>
2026-03-06 10:30:57 +0800

    Add source code files for the Emby Media Manager repository

---
+++ src/database/db.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id = db.Column(db.Integer, primary_key=True)
+    emby_id = db.Column(db.String(255), unique=True, nullable=False, index=True)
+    title = db.Column(db.String(500), nullable=False)
+    media_type = db.Column(db.String(50), nullable=False)
+    path = db.Column(db.Text)
+    size = db.Column(db.BigInteger)
+    duration = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def __repr__(self):
+        return f"<Media {self.title}>"
+
+class SyncLog(db.Model):
+    __tablename__ = 'sync_logs'
+    id = db.Column(db.Integer, primary_key=True)
+    sync_time = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(50))
+    items_synced = db.Column(db.Integer)
+    error_message = db.Column(db.Text)
+    
+    def __repr__(self):
+        return f"<SyncLog {self.sync_time}>"
--- src/emby/__init__.py
+++ src/emby/__init__.py
@@ -1,5 +1,2 @@
-"""Emby Module Initialization"""
-
 from src.emby.client import EmbyClient
-
 __all__ = ['EmbyClient']
